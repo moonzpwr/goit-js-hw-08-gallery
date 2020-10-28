7d805cada047455d1acc5a33e5d9743b89e28e3c
moonzpwr <60944420+moonzpwr@users.noreply.github.com>
2020-10-28 17:30:35 +0200

    new features
    1. scroll by keyboard(without cycle)
    2. scroll by HTML buttons

--- js.js
+++ js.js
@@ -19,12 +19,16 @@ import pictures from "./gallery-items.js";
 const galleeyContainerEL = document.querySelector(".js-gallery");
 const modalContainerEl = document.querySelector(".js-lightbox");
 const bigImageEl = document.querySelector(".lightbox__image");
+const backButtonEl = document.querySelector("#js-back");
+const forwardButtonEl = document.querySelector("#js-forward")
+
+// console.log(backButtonEl, forwardButtonEl);
 
 // ADD HTML
 
 function addElementInGallery(images) {
   let result = [];
-  images.map(function ({ preview, original, description }) {
+  images.map(function ({ preview, original, description }, index) {
     result.push(
       `<li class="gallery__item">
   <a
@@ -35,6 +39,7 @@ function addElementInGallery(images) {
       class="gallery__image"
       src="${preview}"
       data-source="${original}"
+      data-index="${index}"
       alt="${description}"
     />
   </a>
@@ -50,26 +55,36 @@ addElementInGallery(pictures);
 galleeyContainerEL.addEventListener("click", onClickGalleryImg); // add listener to open modal
 
 function onClickGalleryImg(event) {
-  if (event.target.nodeName != "IMG") {
+  if (event.target.nodeName !== "IMG") {
     return;
   }
   event.preventDefault(); // don't go to link
+
+  const { source, index } = event.target.dataset;
+  // console.log(event.target);
+
   modalContainerEl.classList.add("is-open"); // open modal
-  bigImageEl.src = event.target.dataset.source; // change URL to display IMG
+  bigImageEl.src = source; // change URL to display IMG
+  bigImageEl.dataset.index = index; // change index
+  bigImageEl.alt = event.target.alt; // change alt
   window.addEventListener("keydown", closeModalByEsc); //add listener to close modal with ECS
-  window.addEventListener("keydown", keyboarnPress); //add listener to scroll images wish keyboard
+  window.addEventListener("keydown", keyboardPress); //add listener to scroll images wish keyboard
+  window.addEventListener("click", scrollByButtons); //add listener to scroll images wish buttons
 }
 
 // CLOSE MODAL
 modalContainerEl.addEventListener("click", closeModalByMouse); //add listener to close modal with mouse(close button or click on backdrop)
 
 function closeModalByMouse(event) {
-  if (event.target.nodeName != "DIV" && event.target.nodeName != "BUTTON") {
+  if (event.target.nodeName !== "DIV" && event.target.dataset.action !== "close-lightbox") {
     return;
   }
+  
   modalContainerEl.classList.remove("is-open"); //close modal
   bigImageEl.src = ""; //clear URL
   window.removeEventListener("keydown", closeModalByEsc); //remove excess listener when modal close
+  window.removeEventListener("keydown", keyboardPress); //remove listener to scroll images wish keyboard
+  window.removeEventListener("click", scrollByButtons); //remove listener to scroll images wish buttons
 }
 function closeModalByEsc(event) {
   if (event.code != "Escape") {
@@ -78,14 +93,15 @@ function closeModalByEsc(event) {
   modalContainerEl.classList.remove("is-open"); //close modal
   bigImageEl.src = ""; //clear URL
   window.removeEventListener("keydown", closeModalByEsc); //remove excess listener when modal close
-  window.removeEventListener("keydown", keyboarnPress); //add listener to scroll images wish keyboard
+  window.removeEventListener("keydown", keyboardPress); //remove listener to scroll images wish keyboard
+  window.removeEventListener("click", scrollByButtons); //remove listener to scroll images wish buttons
 }
 
-// SCROLLING PICTURES BY KEYBOARD
+// SCROLLING PICTURES BY KEYBOARD with cycle
 
 const UrlsArr = pictures.map((el) => el.original);
 
-function keyboarnPress(event) {
+function keyboardPress(event) {
   if (event.code === "ArrowRight") {
     for (let i = 0; i < UrlsArr.length; i += 1) {
       if (bigImageEl.src === UrlsArr[8]) {
@@ -97,6 +113,55 @@ function keyboarnPress(event) {
       }
     }
   } else if (event.code === "ArrowLeft") {
+    for (let i = 0; i < UrlsArr.length; i += 1) {
+      if (bigImageEl.src === UrlsArr[0]) {
+        bigImageEl.src = `${UrlsArr[8]}`;
+        return;
+      } else if (bigImageEl.src === UrlsArr[i]) {
+        bigImageEl.src = `${UrlsArr[i - 1]}`;
+        return;
+      }
+    }
+  }
+}
+
+// SCROLLING PICTURES BY KEYBOARD without cycle
+
+// const UrlsArr = pictures.map(({ original, description }, i) => ({
+//   href: original,
+//   alt: description,
+//   index: i,
+// }));
+
+// function keyboardPress(event) {
+//   if (event.code === "ArrowRight") {
+//     bigImageEl.src = UrlsArr[Number(bigImageEl.dataset.index) + 1].href;
+//     bigImageEl.dataset.index = Number(bigImageEl.dataset.index) + 1;
+//     bigImageEl.alt = UrlsArr[Number(bigImageEl.dataset.index) + 1].alt;
+//     // console.log(UrlsArr[Number(bigImageEl.dataset.index) + 1].href);
+//   } else if (event.code === "ArrowLeft") {
+//     bigImageEl.src = UrlsArr[Number(bigImageEl.dataset.index) - 1].href;
+//     bigImageEl.dataset.index = Number(bigImageEl.dataset.index) - 1;
+//     bigImageEl.alt = UrlsArr[Number(bigImageEl.dataset.index) - 1].alt;
+//   }
+// }
+
+// SCROLLING PICTURES BY BUTTON on website
+function scrollByButtons(event) { 
+  if (event.target.dataset.action !== "back" && event.target.dataset.action !== "forward") {
+    return;
+  }
+  if (event.target.dataset.action === "forward") {
+    for (let i = 0; i < UrlsArr.length; i += 1) {
+      if (bigImageEl.src === UrlsArr[8]) {
+        bigImageEl.src = `${UrlsArr[0]}`;
+        return;
+      } else if (bigImageEl.src === UrlsArr[i]) {
+        bigImageEl.src = `${UrlsArr[i + 1]}`;
+        return;
+      }
+    }
+  } else if (event.target.dataset.action === "back") {
     for (let i = 0; i < UrlsArr.length; i += 1) {
       if (bigImageEl.src === UrlsArr[0]) {
         bigImageEl.src = `${UrlsArr[8]}`;
